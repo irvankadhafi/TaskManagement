b32973c62ba35bc6b18b7693a8ad16db770decf1
Irvan Kadhafi <irvankadhafi04@gmail.com>
2025-05-04 22:42:48 +0700

    remove unused files

--- tests/TaskManagement.Application.Tests/obj/Debug/net8.0/TaskManagement.Application.Tests.AssemblyInfo.cs
+++ tests/TaskManagement.Application.Tests/obj/Debug/net8.0/TaskManagement.Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14bbc884bc283b71d7292afb3fc7a84ca3e3b281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492d2d50d0b0e4e30fe4838fef7d2c1f8b3535b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
